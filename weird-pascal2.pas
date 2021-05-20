47b87f1d5e706ec448ab7d4f5485db068c983df4
Christopher Ray <vde435@gmail.com>
2021-05-20 06:17:59 -0500

    Create weird-pascal2.pas

---
+++ weird-pascal2.pas
@@ -0,0 +1,5 @@
+if, left round bracket, x equal equal true, right round bracket
+left curly brace
+print formatted, left round bracket, inch-sign, hello world,
+     backslash n, inch-sign, right round bracket, semicolon
+right curly brace
